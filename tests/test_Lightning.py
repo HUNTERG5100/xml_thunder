e5af4726b73c5b46e195767a699f673354e8325b
HUNTERG5100 <hunterg5100@gmail.com>
2021-06-09 18:43:38 -0500

    Added unittests for get_all_routes and route; Removed dunder tests

--- tests/test_Lightning.py
+++ tests/test_Lightning.py
@@ -7,16 +7,57 @@ xml_parser = Lightning()
 
 @xml_parser.route("foo")
 def foo(element):
-    print("foo")
+    pass
+
+
+def bar(element):
+    pass
 
 
 class TestLightning(unittest.TestCase):
-    def test_getitem(self):
+    def test_get_all_routes(self):
         """
-        Tests that the __getitem__ method returns an object or None
+        Tests that the get_all_routes method
+        returns all routes with intended items
         """
 
-        self.assertEqual(xml_parser["foo"], foo)
+        # Checks routes before adding extra route
+        self.assertEqual(xml_parser.get_all_routes(), {"foo": foo})
+
+        # Adds a route
+        xml_parser["bar"] = bar
+
+        # Checks routes after adding extra route
+        self.assertEqual(xml_parser.get_all_routes(), {"foo": foo, "bar": bar})
+
+        # Deletes added route
+        del xml_parser["bar"]
+
+        # Checks routes after deleting extra route
+        self.assertEqual(xml_parser.get_all_routes(), {"foo": foo})
+
+    def test_route(self):
+        """
+        Tests that the route decorator registers a route
+        """
+
+        # Checks routes before changes
+        self.assertEqual(xml_parser.get_all_routes(), {"foo": foo})
+
+        # Adds a new route through decoration
+        @xml_parser.route("foobar")
+        def foobar(element):
+            pass
+
+        # Checks routes after changes
+        self.assertEqual(
+            xml_parser.get_all_routes(), {"foo": foo, "foobar": foobar})
+
+        # Deletes newly added route
+        del xml_parser["foobar"]
+
+        # Checks routes after deleting extra route
+        self.assertEqual(xml_parser.get_all_routes(), {"foo": foo})
 
 
 if __name__ == '__main__':
